840b709d8d126aef8c0c32159ec8137129b9db8a
batic420 <b.ludwig089765@gmail.com>
2025-01-08 22:56:00 +0100

    implement first orm-build & entity

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import commonMiddleware from "./middlewares/middleware";
 import routes from "./routes/routes";
 import { dbConfig } from "./config/database";
+import { AppDataSource } from "./config/appDataSource";
 
 dotenv.config();
 
@@ -11,8 +12,15 @@ const port = process.env.PORT || 3000;
 const db = dbConfig.init();
 commonMiddleware(app);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+    
+    app.use("/", routes);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.use("/", routes);
