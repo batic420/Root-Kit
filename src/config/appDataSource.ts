ee508629147de71cb6c51069b37fe6024b0785e5
batic420 <b.ludwig089765@gmail.com>
2025-02-04 20:55:01 +0100

    switch from local mysql instance to docker one -> CLI Tool

--- src/config/appDataSource.ts
+++ src/config/appDataSource.ts
@@ -1,19 +1,23 @@
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 import { User } from '../database/entities';
+import { parse } from 'path';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT || '3306'),
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     synchronize: true,
     logging: true,
     entities: [
         User
     ],
-})
+    extra: {
+        connectionLimit: 10
+    }
+});
